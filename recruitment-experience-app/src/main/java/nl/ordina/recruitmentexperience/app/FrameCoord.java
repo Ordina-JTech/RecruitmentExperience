14e2cac6ad858bbd920a32596f1bf980ef4a043c
Tjebbe Kerstens <t.kerstens@live.nl>
2019-06-20 16:36:05 +0200

    kapotte transform

---
+++ recruitment-experience-app/src/main/java/nl/ordina/recruitmentexperience/app/FrameCoord.java
@@ -0,0 +1,19 @@
+package nl.ordina.recruitmentexperience.app;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FrameCoord {
+
+    private int x;
+    private int y;
+
+}
--- recruitment-experience-app/src/main/java/nl/ordina/recruitmentexperience/app/PdfToImage.java
+++ recruitment-experience-app/src/main/java/nl/ordina/recruitmentexperience/app/PdfToImage.java
@@ -1,5 +1,6 @@
 package nl.ordina.recruitmentexperience.app;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.PDFRenderer;
@@ -10,24 +11,46 @@ import org.springframework.util.ResourceUtils;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
 public class PdfToImage implements CommandLineRunner {
 
+    private FrameData[] frameDatas;
+
     @Override
     public void run(String... strings) {
         try {
-            renderPdfToImage();
+            frameDatas = jsonToFrameData();
+            BufferedImage bufferedImage = renderPdfToImage();
+            for(int i = 0; i < 53; i++) {
+                renderImageIntoImage(bufferedImage, i + 1);
+            }
         } catch (IOException e) {
-            //intentionally left empty
+            e.printStackTrace();
         }
     }
 
-    private void renderPdfToImage() throws IOException {
+    private FrameData[] jsonToFrameData() throws IOException {
+        File file = ResourceUtils.getFile("classpath:frames/framedata.json");
+        String json = new String(Files.readAllBytes(file.toPath()));
+
+        ObjectMapper mapper = new ObjectMapper();
+        FrameData[] frameDatas = mapper.readValue(json, FrameData[].class);
+        return frameDatas;
+    }
+
+    private BufferedImage renderPdfToImage() throws IOException {
         File file = ResourceUtils.getFile("classpath:testcv.pdf");
 
         PDDocument document = PDDocument.load(file);
@@ -36,23 +59,80 @@ public class PdfToImage implements CommandLineRunner {
         BufferedImage bim = pdfRenderer.renderImageWithDPI(0, 300);
         document.close();
 
-        renderImageIntoImage(bim);
+        return bim;
     }
 
-    private void renderImageIntoImage(BufferedImage bim) throws IOException {
-        File backgroundFile = ResourceUtils.getFile("classpath:background.jpg");
+    private void renderImageIntoImage(BufferedImage bim, final int frameNumber) throws IOException {
+        String number = frameNumber < 10 ? "0"+frameNumber : Integer.toString(frameNumber);
+        File backgroundFile = ResourceUtils.getFile(String.format("classpath:frames/thumb%s.jpg", number));
         BufferedImage background = ImageIO.read(backgroundFile);
 
+        FrameData current = frameDatas[frameNumber-1];
+        FrameCoord tl = current.getTopLeft();
+        FrameCoord tr = current.getTopRight();
+        FrameCoord bl = current.getBottomLeft();
+        FrameCoord br = current.getBottomRight();
+
         Graphics2D g = background.createGraphics();
-        g.setRenderingHints(new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON));
-        g.setRenderingHints(new RenderingHints(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON));
-        g.setRenderingHints(new RenderingHints(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY));
-        g.rotate(Math.toRadians(-2.9));
-        g.drawImage(bim, 395,
-                167, 498, 755, null);
+
+        Polygon polygon = new Polygon();
+        polygon.addPoint(tl.getX(), tl.getY());
+        polygon.addPoint(tr.getX(), tr.getY());
+        polygon.addPoint(br.getX(), br.getY());
+        polygon.addPoint(bl.getX(), bl.getY());
+
+
+        g.clip(polygon);
+
+        AffineTransform affineTransform = new AffineTransform();
+
+
+
+        g.draw(affineTransform.createTransformedShape(polygon));
+
+        g.drawImage(bim, affineTransform, null);
+
         g.dispose();
 
-        ImageIOUtil.writeImage(background, "final.jpg", 300);
+        ImageIOUtil.writeImage(background, String.format("processed/frame%s.jpg", number), 300);
+
+//        generateVideoFromFrame();
+    }
+
+    private void generateVideoFromFrame() {
+        ProcessBuilder createVid = new ProcessBuilder("ffmpeg", "-start_number", "1", "-t", "1", "-i", "processed/frame%02d.jpg", "processed.mp4");
+        try {
+            Process createProcess = createVid.start();
+
+            System.out.println(readOutput(createProcess.getErrorStream()));
+            System.out.println(readOutput(createProcess.getInputStream()));
+
+//            concatVideos();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void concatVideos() {
+        ProcessBuilder concatVid = new ProcessBuilder("ffmpeg", "-f", "concat", "-i", "list.txt", "-auto_convert", "1", "-c", "copy", "final.mp4");
+        try {
+            Process concatProcess = concatVid.start();
+
+            System.out.println(readOutput(concatProcess.getErrorStream()));
+            System.out.println(readOutput(concatProcess.getInputStream()));
+
+
+            System.out.println("klaar");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private List<String> readOutput(InputStream inputStream) throws IOException {
+        try (BufferedReader output = new BufferedReader(new InputStreamReader(inputStream))) {
+            return output.lines()
+                    .collect(Collectors.toList());
+        }
     }
 }
 
